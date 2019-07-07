ef98faab41f299f21b9a826876d8b3153372a612
Nazariy Skoropad <nazar.skoropad@icloud.com>
2019-07-07 21:35:26 +0300

    Add files via upload

---
+++ FL11_HW6/homework/js/task1.js
@@ -0,0 +1,9 @@
+// Your code goes here
+const a1 = Number( prompt('Please enter a1') );
+const a2 = Number( prompt('Please enter a2') );
+const b1 = Number( prompt('Please enter b1') );
+const b2 = Number( prompt('Please enter b2') );
+const c1 = Number( prompt('Please enter c1') );
+const c2 = Number( prompt('Please enter c2') );
+const denominator = 2;
+console.log((a1+b1)/denominator === c1 && (a2+b2)/denominator === c2);
---
+++ FL11_HW6/homework/js/task2.js
@@ -0,0 +1,15 @@
+// Your code goes here
+const a = Number( prompt('Please enter a') );
+const b = Number( prompt('Please enter b') );
+const c = Number( prompt('Please enter c') );
+if (a+b>c && a+c>b && c+b>a) {
+	if (a===b && b===c){	
+			alert('Equivalent triangle'); 
+	}	else if (a===b || b===c || a===c) {	
+			alert('Isosceles triangle');
+	}	else{ 
+			alert('Normal triangle'); 
+		} 
+}	else{
+		alert('Triangle doesn’t exist');
+	}
